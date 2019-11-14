3382fdb991719f9a98d3f71f521cb5534487953e
Roberto Atanásio <roberto.atanasio.pl@gmail.com>
2019-11-14 19:14:17 -0300

    admin pages

--- src/components/admin/AdminPages.vue
+++ src/components/admin/AdminPages.vue
@@ -1,15 +1,34 @@
 <template>
     <div class="admin-pages">
         <PageTitle icon="fa fa-cogs" main="Administração do Sistema" sub="Cadastros & Cia" />
+        <!-- os tags b- se refrem ao bootstarp-vue - tabs -->
+        <b-card no-body>
+            <b-tabs card>
+                <b-tab title="Artigos" active>
+                    <ArticleAdmin />
+                </b-tab>
+                <b-tab title="Categorias">
+                    <CategoryAdmin />
+                </b-tab>
+                <b-tab title="Usuários">
+                    <!-- <Teste /> -->
+                    <UserAdmin />
+                </b-tab>
+            </b-tabs>
+        </b-card>
     </div>
 </template>
 
 <script>
 import PageTitle from '../template/PageTitle'
+import ArticleAdmin from './ArticlesAdmin'
+import CategoryAdmin from './CategoryAdmin'
+import UserAdmin from './UserAdmin'
+import Teste from './Teste'
 
 export default {
     name: 'AdminPages',
-    components: { PageTitle }
+    components: { PageTitle, ArticleAdmin, CategoryAdmin, UserAdmin, Teste }
 }
 </script>
 
---
+++ src/components/admin/Teste.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <b-table :fields="fields" :items="items" foot-clone>
+
+      <!-- <template v-slot:cell(name)="data">
+        {{ data.value.first }} {{ data.value.last }}
+      </template>
+
+      <template v-slot:head(name)="data">
+        <span class="text-info">{{ data.label.toUpperCase() }}</span>
+      </template>
+
+      <template v-slot:foot(name)="data">
+        <span class="text-danger">{{ data.label }}</span>
+      </template>
+
+      <template v-slot:foot()="data">
+        <i>{{ data.label }}</i>
+      </template> -->
+    </b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        fields: [
+          // A column that needs custom formatting
+          { key: 'name', label: 'Full Name' },
+          // A regular column
+          'age',
+          // A regular column
+          'sex'
+        ],
+        items: [
+          { name: { first: 'John', last: 'Doe' }, sex: 'Male', age: 42 },
+          { name: { first: 'Jane', last: 'Doe' }, sex: 'Female', age: 36 },
+          { name: { first: 'Rubin', last: 'Kincade' }, sex: 'Male', age: 73 },
+          { name: { first: 'Shirley', last: 'Partridge' }, sex: 'Female', age: 62 }
+        ]
+      }
+    }
+  }
+</script>
